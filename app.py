2dc679c3c3694275b7b04e0b9f7fbc80e89069c6
marcoshffb <mhenriqueffb@gmail.com>
2025-05-14 21:33:35 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def ola():
+    return render_template('index.html')
+
+@app.route('/sobre_equipe')
+def equipe():
+    return render_template('sobre_equipe.html')
+
+app.run()
